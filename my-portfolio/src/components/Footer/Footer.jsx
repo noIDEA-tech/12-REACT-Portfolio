8f86cf935ec6ec4349d6c3b837c81a51334a42ef
Nancy Watreas <nwatreas2023@gmail.com>
2025-01-05 12:14:46 -0600

    add Footer component with social media links and create About Me page layout

--- my-portfolio/src/components/Footer/Footer.jsx
+++ my-portfolio/src/components/Footer/Footer.jsx
@@ -1,1 +1,51 @@
 //For Social Media links and additonal information//
+import { GithubIcon, LinkedinIcon, TwitterIcon } from 'lucide-react';
+
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white py-6 mt-auto">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-center space-x-6">
+
+             {/* GitHub Link */}
+            <a 
+                href="https://github.com/noIDEA-tech"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-400 transition-colors"
+            >
+                <GithubIcon className="w-6 h-6" />
+                <span className="sr-only">GitHub</span>
+            </a>
+
+            {/* LinkedIn Link */}
+            <a 
+                href="https://linkedin.com/in/nancy-watreas" //TODO: GET CORRECT LINK
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-400 transition-colors"
+            >
+                <LinkedinIcon className="w-6 h-6" />
+                <span className="sr-only">LinkedIn</span>
+            </a>
+
+            {/* Twitter/X Link*/}
+            <a
+                href="https://twitter.com/NancyWatreas2023@NWatreas2023"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-400 transition-colors"
+            >
+                <TwitterIcon className="w-6 h-6" />
+                <span className="sr-only">Twitter X</span>
+            </a>
+        </div>
+        <p  className="text-center mt-4 text-sm">
+            © {new Date().getFullYear()} Nancy Watreas. All rights reserved.  
+        </p>
+    </div>
+    </footer>
+    );
+};
+
+export default Footer;
---
+++ my-portfolio/src/pages/AboutMe/AboutMe.jsx
@@ -0,0 +1,16 @@
+const AboutMe = () => {
+    return (
+        <div className="max-w-4xl mx-auto px-4">
+            <h1 className="text-3xl font-bold md-8">About Me</h1>
+
+            <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
+                {/* Profile Image */}
+                <div className="w-48 h-48 rounded-full overflow-hidden shadow-lg">
+                    <img
+                    src=""
+                </div>
+            </div>
+        </div>
+    )
+}
+    
