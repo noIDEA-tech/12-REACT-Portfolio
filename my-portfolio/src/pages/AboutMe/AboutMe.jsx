22da53715efb434133c193c5feea707312d21621
Nancy Watreas <nwatreas2023@gmail.com>
2025-01-06 10:55:05 -0600

    add Resume PDF and enhance About Me section with professional background and personal interests

--- my-portfolio/src/pages/AboutMe/AboutMe.jsx
+++ my-portfolio/src/pages/AboutMe/AboutMe.jsx
@@ -17,14 +17,28 @@ const AboutMe = () => {
             <div className="flex-1">
                 <p className="text-lg mb-4">
                 {/* TODO: Add bio content here */}
-                </p>
+                </p> My professional career spans over 25 years working for two Fortune 500 companies, Electrolux
+                     subsidiary Dometic Corporation and Travelers Property Insurance, in the capacity of administrative
+                     assistant, office manager and purchasing agent; and commercial and residential property adjuster,
+                     respectively. In addition, I served as Principal Real Estate Broker/part-owner of an Indiana Real
+                     Estate Brokerage company. My belief in being proactive as well as my intuative nature for 
+                     problem-solving has led to my pursute in studying computer technology, specifically front-end web 
+                     development. 
 
                 <p className="text-lg mb-4">
                   {/* TODO: Add background and work history content here */}   
                 </p>
+                <p className="text-lg mb-4">
+                    My background in administrative roles has equipped me with strong organizational and multitasking skills. 
+                    During my tenure at Dometic Corporation, I managed office operations, coordinated events, and handled 
+                    procurement processes. At Travelers Property Insurance, I honed my analytical skills by assessing 
+                    property damage and negotiating settlements. My experience in real estate has provided me with a 
+                    comprehensive understanding of property management and client relations.
+                </p>
 
                 <p className="text-lg">
                     {/*TODO: Add other interests, hobbies, etc here */}
+                    Outside of my professional life, I have a passion for gardening and landscaping. I also love traveling and exploring new cultures, which has broadened my perspective and appreciation for cultural differences. In my free time, I enjoy spending quality time with my family and engaging in physical fitness activities.
                 </p>
             </div>
         </div>
