ed63a13b0b6bf1a8f81b235142c189892f2432bc
Nancy Watreas <nwatreas2023@gmail.com>
2025-01-06 17:54:24 -0600

    fix: correct image import for README.md Generator in Portfolio component

--- my-portfolio/src/pages/Portfolio/Portfolio.jsx
+++ my-portfolio/src/pages/Portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import weatherDashboard from '../../assets/images/weather-dashboard.jpg.png';
 import employeeTracker from '../../assets/images/employee-tracker.jpg';
 import sleepTracker from '../../assets/images/sleep-tracker.png';
 import vehicleBuilder from '../../assets/images/vehicle-builder.jpg';
-// import readMeGenerator from '../../assets/images/readme-generator.jpg';
+import readMeGenerator from '../../assets/images/readme-generator.jpg.png';
  
 
 
@@ -35,12 +35,12 @@ const Portfolio = () => {
             githubLink: "https://github.com/noIDEA-tech/08-TS-Vehicle-Builder/tree/main/Develop",
             description: "TypeScript command-line application that builds and uses cars to have additional options for motorbikes and trucks"  
          },
-        // {
-        //     title: "README.md Generator",
-        //     image:  readMeGenerator,  //TODO: ADD IMAGE, make sure image name matches your file            
-        //     githubLink: "https://github.com/noIDEA-tech/README.md-Generator", //TODO: insert link to 5th project
-        //     description: "A command-line README.md generator designed for users to easily create README.md project files"
-        // },
+        {
+            title: "README.md Generator",
+            image:  readMeGenerator,  //TODO: ADD IMAGE, make sure image name matches your file            
+            githubLink: "https://github.com/noIDEA-tech/README.md-Generator", //TODO: insert link to 5th project
+            description: "A command-line README.md generator designed for users to easily create README.md project files"
+        },
     ];
 
     return (
